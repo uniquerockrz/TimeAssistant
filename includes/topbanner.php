b72e6960656851f745d26dcb346ab833e4ae59f1
Bhushan shah <bhush94@gmail.com>
2012-12-15 18:18:33 +0530

    Fixed issue with internal linking

--- includes/topbanner.php
+++ includes/topbanner.php
@@ -7,13 +7,18 @@ else
 version 0.3-unstable
 <p align="center"><font face="comic sans ms">I am your boss, I tell you what to do :-P</font></p>
 <fieldset>
-<font face="georgia">
-<a href="index.php" >&nbsp;Home&nbsp;</a> |
-<a href="plans/plans.php" >&nbsp;Plans&nbsp;</a> |
-<a href="events/events.php" >&nbsp;Events&nbsp;</a> |
-<a href="todo/todo.php" >&nbsp;TODO&nbsp;</a> |
-<a href="categories/categories.php" >&nbsp;Categories&nbsp;</a> |
-<a href="timetable/timetable.php" >&nbsp;Time Table&nbsp;</a>
+<font face="georgia"> 
+<?php if($index==false) 
+	$tmp="../";
+else 
+	$tmp="./";
+?>
+<a href="<?php echo $tmp; ?>index.php" >&nbsp;Home&nbsp;</a> |
+<a href="<?php echo $tmp; ?>plans/plans.php" >&nbsp;Plans&nbsp;</a> |
+<a href="<?php echo $tmp; ?>events/events.php" >&nbsp;Events&nbsp;</a> |
+<a href="<?php echo $tmp; ?>todo/todo.php" >&nbsp;TODO&nbsp;</a> |
+<a href="<?php echo $tmp; ?>categories/categories.php" >&nbsp;Categories&nbsp;</a> |
+<a href="<?php echo $tmp; ?>timetable/timetable.php" >&nbsp;Time Table&nbsp;</a>
 </font>
 </fieldset>
 </center>
