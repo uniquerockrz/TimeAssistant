00faa0d0b0eda7ecb7b1fed608d0c624a84b86cd
Bhushan shah <bhush94@gmail.com>
2012-12-15 17:52:09 +0530

    Issue related to logo solved

--- includes/topbanner.php
+++ includes/topbanner.php
@@ -1,15 +1,19 @@
 <center>
-<img src="http://saurav.modak/~srvmdk/timeassistant/includes/logo.jpg" align="center"/></br>
+<?php if($index == true) 
+	echo '<img src="includes/logo.jpg" align="center"/></br>';
+else 
+	echo '<img src="../includes/logo.jpg" align="center"/></br>';
+?>
 version 0.3-unstable
 <p align="center"><font face="comic sans ms">I am your boss, I tell you what to do :-P</font></p>
 <fieldset>
 <font face="georgia">
 <a href="index.php" >&nbsp;Home&nbsp;</a> |
-<a href="../plans/plans.php" >&nbsp;Plans&nbsp;</a> |
-<a href="../events/events.php" >&nbsp;Events&nbsp;</a> |
-<a href="../todo/todo.php" >&nbsp;TODO&nbsp;</a> |
-<a href="../categories/categories.php" >&nbsp;Categories&nbsp;</a> |
-<a href="../timetable/timetable.php" >&nbsp;Time Table&nbsp;</a>
+<a href="plans/plans.php" >&nbsp;Plans&nbsp;</a> |
+<a href="events/events.php" >&nbsp;Events&nbsp;</a> |
+<a href="todo/todo.php" >&nbsp;TODO&nbsp;</a> |
+<a href="categories/categories.php" >&nbsp;Categories&nbsp;</a> |
+<a href="timetable/timetable.php" >&nbsp;Time Table&nbsp;</a>
 </font>
 </fieldset>
 </center>
